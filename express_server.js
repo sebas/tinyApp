eb45879db90582047931d9613c375870f8f4e992
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-04 21:41:56 +0000

    We how now routes for showing the URLs in the database and the details of a shortened URL given the shortenURL.

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,11 +14,30 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {};
+  if( urlDatabase[req.params.id] === undefined ) {
+    console.log(req.params.id);
+    console.log(urlDatabase[req.params.id]);
+    templateVars = { shortURL: "Not found", longURL: "Not found" };
+  } else {
+    console.log(req.params.id);
+    console.log(urlDatabase[req.params.id]);
+    templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  }
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
