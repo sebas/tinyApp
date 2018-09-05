bb24f2c2e022893b680fe7cdcd0f0be80708b02c
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-05 01:46:20 +0000

    We can now create new short URLs

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+"use strict";
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -9,17 +12,43 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function generateRandomString(howMany = 6) {
+  let rndStr = "";
+  const charset = '0123456789ABCDEFGHIJLKNOPQRSTVYXZabcdefghiklmnopqrstvyzx';
+
+  for (let i = 0; i < howMany; i++) {
+    const index = getRandomInt(charset.length);
+    rndStr += charset.charAt(index);
+  }
+
+  return rndStr;
+}
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.post("/urls", (req, res) => {
+
+  let objectKey = generateRandomString();
+  let longURL = req.body.longURL; // TODO what is we dont have a long url
+  urlDatabase[objectKey] = longURL;
+  console.log("base: ",  );
+  console.log(urlDatabase);
+  res.redirect(req.protocol + "://" + req.get('host') + "/urls/" + objectKey);      
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -28,6 +57,7 @@ app.get("/urls/:id", (req, res) => {
     console.log(req.params.id);
     console.log(urlDatabase[req.params.id]);
     templateVars = { shortURL: "Not found", longURL: "Not found" };
+    templateVars = { error: "URL was not found" };
   } else {
     console.log(req.params.id);
     console.log(urlDatabase[req.params.id]);
@@ -36,8 +66,15 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  if (longURL === undefined) {
+    res.send("oh lordy, why are you like this?  why you do?", req.params.shortURL);
+  } else {
+    console.log(req.params.shortURL);
+    console.log(urlDatabase[req.params.shortURL]);
+    res.redirect(longURL);
+  }
 });
 
 app.listen(PORT, () => {
