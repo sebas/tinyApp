475f0bf1d488bd3ce6b63ebd3b9cd8061321c215
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-05 22:14:09 +0000

    We added a library to facilitaty the use of cookies. We added login and logout capabilities.

--- express_server.js
+++ express_server.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -33,16 +35,27 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/notFound", (req, res) => {
   res.render("url_notFound");
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect(req.protocol + "://" + req.get('host') + "/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect(req.protocol + "://" + req.get('host') + "/urls");
 });
 
 app.post("/urls/:id/delete", (req, res) => {
@@ -65,7 +78,7 @@ app.post("/urls/:id", (req, res) => {
     console.log(req.params.id);
     res.redirect(req.protocol + "://" + req.get('host') + "/notFound");
   } else {
-    let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+    let templateVars = { username: req.cookies["username"], shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render("urls_show", templateVars);
   }
 });
@@ -85,7 +98,7 @@ app.get("/urls/:id", (req, res) => {
     console.log(req.params.id);
     res.redirect(req.protocol + "://" + req.get('host') + "/notFound");
   } else {
-    templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+    templateVars = { username: req.cookies["username"], shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render("urls_show", templateVars);
   }
 });
