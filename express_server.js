74bcb1eed25887f971a2d0709e965ee0aeceac24
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-07 22:48:02 +0000

    Commented the code, removed console.logs.

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 "use strict";
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
@@ -58,6 +58,8 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+// Returns a random string of 6 characters by default, could receive the number
+// of characters to return a string of that size.
 function generateRandomString(howMany = 6) {
   let rndStr = "";
   const charset = '0123456789ABCDEFGHIJLKNOPQRSTVYXZabcdefghiklmnopqrstvyzx';
@@ -70,13 +72,13 @@ function generateRandomString(howMany = 6) {
   return rndStr;
 }
 
+// Returns if an email address is already on the database
+// to avoid duplicate users
 function userEmailAlreadyExists(email) {
   let found = false;
   for (const userKey in users) {
     const user = users[userKey];
-    console.log(`${email} vs ${user.id}: ${user.email} - ${user.password} loop`);
     if(user.email === email) {
-      console.log(`True ${email} - ${user.email} all`);
       found=true;
     }
   }
@@ -84,12 +86,13 @@ function userEmailAlreadyExists(email) {
   return found;
 }
 
+// Returns true if the password from the login form
+// matches the one in the database, otherwise returns
+// false
 function userPasswordMatches(password) {
   let found = false;
   for (const userKey in users) {
     const user = users[userKey];
-    console.log(`${password} vs ${user.id}: ${user.email} - ${user.password} loop`);
-    // bcrypt.compareSync("purple-monkey-dinosaur", hashedPassword)
     if(bcrypt.compareSync(password, user.password)) {
       found=true;
     }
@@ -98,13 +101,13 @@ function userPasswordMatches(password) {
   return found;
 }
 
+// Returns the user id from the database given
+// an email address, otherwise returns an empty string
 function userGetUserIDFromEmail(email){
   let user_id = '';
   for (const userKey in users) {
     const user = users[userKey];
-    console.log(`${email} vs ${user.id}: ${user.email} - ${user.password} loop`);
     if(user.email === email) {
-      console.log(`True ${email} - ${user.email} all`);
       user_id=user.id;
     }
   }
@@ -112,13 +115,13 @@ function userGetUserIDFromEmail(email){
   return user_id;
 }
 
+// Returns true if a user is the owner of an URL in
+// the database
 const owns = function userOwnsURL(user_id, urlGiven) {
   let ownsIt = false;
   for (const urlKey in urlDatabase) {
     const url = urlDatabase[urlKey];
-    console.log(`${user_id} vs ${urlGiven}: ${url.id} - ${url.longURL} loop`);
     if(user_id === url.userID && urlGiven === url.id) {
-      console.log(`True ${user_id} - ${url.userID} and ${urlGiven} - ${url.id} all`);
       ownsIt=true;
     }
   }
@@ -126,31 +129,26 @@ const owns = function userOwnsURL(user_id, urlGiven) {
   return ownsIt;  
 };
 
+// Returns a object containg all the URLs
+// a user  has in the database otherwise
+// an empty object
 const uURLs = function urlsForUser(user_id) {
   let ownsIt = {};
   for (const urlKey in urlDatabase) {
     const url = urlDatabase[urlKey];
-    console.log(`${user_id} loop`);
     if(user_id === url.userID) {
-      console.log(`True ${user_id} - ${url.userID} all`);
       ownsIt[urlKey] = { id: url.id, longURL: url.longURL, userID: url.userID};
     }
   }
 
-  console.log(`${ownsIt} return`);
-  console.log(ownsIt);
   return ownsIt;
 };
 
 // The home page, it redirects depending on a user being logged or not. 
 app.get("/", (req, res) => {
-  console.log("cookie session", req.session);
-  console.log("cookie user_id", req.body.user_id);
   if(req.body.user_id === undefined) {
-    console.log("going for the login form");
     res.redirect(req.protocol + "://" + req.get('host') + "/login");
   } else {
-    console.log("going for the URLs list");
     res.redirect(req.protocol + "://" + req.get('host') + "/urls");
   }
 });
@@ -161,10 +159,10 @@ app.get("/register", (req, res) => {
 });
 
 // Add a new user to our database.
+// Handles cases as email(user) already exists
+// or submited form was missing something.
 app.post("/register", (req, res) => {
-  console.log(`${req.body.email} - ${req.body.password} register post`);
   if(req.body.email === "" || req.body.password === "" ) {
-    console.log("empty something... redirecting back");
     res.status(400).send('Sorry, you forgot to fill something, please go back and try again!');
   } else if (userEmailAlreadyExists(req.body.email) ) {
     // email already on the database... redirect
@@ -173,7 +171,7 @@ app.post("/register", (req, res) => {
     let userKey = generateRandomString();
     let email = req.body.email;
     let password = bcrypt.hashSync(req.body.password, 10);;
-    // Adding an object to an object
+    // Adding an object to an object (note to self)
     let newUser = { 
       id: userKey,
       email: email,
@@ -181,29 +179,29 @@ app.post("/register", (req, res) => {
     };
     users[userKey] = newUser;
 
-    console.log("New users looks like: ", userKey, email, password );
-    console.log("Users database: ",  users);
     req.session.user_id = userKey;
     res.redirect(req.protocol + "://" + req.get('host') + "/urls");
   }
 });
+
 
 app.get("/notFound", (req, res) => {
   const user = users[req.session.user_id];  
   res.render("url_notFound", {user: user});
 });
 
+// The form to do a login
 app.get("/login", (req, res) => {
-  console.log("login cookie user_id", req.session.user_id);
   res.render("login");
 });
 
+// Validates a user login
+// sets cookie and/or redirects accordingly
 app.post("/login", (req, res) => {
   if (!userEmailAlreadyExists(req.body.email) ) {
     res.redirect(403, req.protocol + "://" + req.get('host') + "/login");
   } else if (userPasswordMatches(req.body.password) ) {
     const user_id = userGetUserIDFromEmail(req.body.email);
-    console.log("login(post) cookie user_id and user_id", req.body.user_id, user_id);
     req.session.user_id = user_id;
     res.redirect(req.protocol + "://" + req.get('host') + "/urls");
   } else {
@@ -211,36 +209,38 @@ app.post("/login", (req, res) => {
   }
 });
 
+// Logs out a user destorying the session (GET version)
 app.get("/logout", (req, res) => {
   req.session = null;
   res.redirect("/login");
 });
 
+// Logs out a user destorying the session (POST version)
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect(req.protocol + "://" + req.get('host') + "/");
 });
 
+// Returns the whole database in JSON format
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Show the list of the URLs in the database
+// that belong to the user that is currently in session
 app.get("/urls", (req, res) => {
   const user = users[req.session.user_id];
-  console.log('user', user);
   const templateVars = { user: user, urls: uURLs(req.session.user_id) };
   res.render("urls_index", templateVars);
 });
 
+// 
 app.get("/urls/new", (req, res) => {
-  console.log('user_id', req.session.user_id);
   if(req.session.user_id === undefined) {
-    console.log("going for the login form");
     res.redirect(req.protocol + "://" + req.get('host') + "/login");
   } else {
     const user = users[req.session.user_id];
     let templateVars = { user: user };
-    console.log('id and user', req.session.user_id, user);
     res.render("urls_new", templateVars);
   }
 });
@@ -248,13 +248,10 @@ app.get("/urls/new", (req, res) => {
 // Delete an URL from the database if conditions are met. 
 app.delete("/urls/:id/delete", (req, res) => {
   if( urlDatabase[req.params.id] === undefined ) {
-    console.log(req.params.id);
     res.redirect(req.protocol + "://" + req.get('host') + "/notFound");
   } else {
     if(owns(req.session.user_id, req.params.id)) {
-      console.log(req.params.id);
       delete urlDatabase[req.params.id];
-      console.log(urlDatabase);
       res.redirect(req.protocol + "://" + req.get('host') + "/urls");
     } else {
       res.status(403).send('Sorry, only the owner can delete a URL!');
@@ -269,7 +266,6 @@ app.put("/urls/:id", (req, res) => {
     if( longURL === '') {
       res.status(400).send('Sorry, long URL should not be empty!');
     } else {
-      console.log("Updating longURL", longURL);
       urlDatabase[req.params.id].longURL = longURL;
       res.redirect("/urls");
     }
@@ -278,6 +274,7 @@ app.put("/urls/:id", (req, res) => {
   }
 });
 
+// The form to edit an URL
 app.get("/urls/:id", (req, res) => {
   if(owns(req.session.user_id, req.params.id)) {
     const user = users[req.session.user_id];
@@ -288,20 +285,17 @@ app.get("/urls/:id", (req, res) => {
   }
 });
 
+// Add a new url to the database
 app.post("/urls", (req, res) => {
-  // Add a new url to the database
-  console.log("Add a new url to the database");
   let objectKey = generateRandomString();
   let longURL = req.body.longURL; // TODO what is we don't have a long url
   let newURL = {id: objectKey, longURL: longURL, userID: req.session.user_id};
   urlDatabase[objectKey] = newURL;
-  console.log("URL database: ",  urlDatabase);
   res.redirect(req.protocol + "://" + req.get('host') + "/urls/" + objectKey);      
 });
 
+// Redirects to the long URL corresponding to the short URL given
 app.get("/u/:shortURL", (req, res) => {
-  console.log("shortURL", req.params.shortURL );
-  console.log("longURL", urlDatabase[req.params.shortURL]  );
   if(urlDatabase[req.params.shortURL] === undefined) { // It is not in the database.
     res.redirect(req.protocol + "://" + req.get('host') + "/notFound");
   } else {
@@ -309,13 +303,11 @@ app.get("/u/:shortURL", (req, res) => {
       if (longURL === undefined) {
         res.redirect(req.protocol + "://" + req.get('host') + "/notFound");
       } else {
-        console.log(req.params.shortURL);
-        console.log(urlDatabase[req.params.shortURL]);
         res.redirect(longURL);
       }
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp app listening on port ${PORT}!`);
 });
